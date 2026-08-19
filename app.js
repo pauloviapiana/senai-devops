3ee8b8aab48744f1cb354e35a8b1cd70880d5337
PAULO HENRIQUE VIAPIANA <paulo_viapiana@estudante.sesisenai.org.br>
2026-08-18 21:39:38 -0300

    fix: adiciona break quando o chute é igual ao numero secreto e corrige o sinal para maior quando o chute é maior que o número secreto

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ let tentativas = 1;
 while (chute != numeroSecreto) {
     chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
     if (chute == numeroSecreto) {
+        break;
     } else {
-        if (chute < numeroSecreto) {
+        if (chute > numeroSecreto) {
             alert(`O número secreto é menor que ${chute}`)
         } else {
             alert(`O número secreto é maior que ${chute}`)
