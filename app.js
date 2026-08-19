c7fa889a69edba9a7ebe66385c6f6d7ede304928
gucardosu <gustavocardosodot@gmail.com>
2026-08-18 21:44:52 -0300

    feat: adiciona novas tentativas

--- app.js
+++ app.js
@@ -1,23 +1,34 @@
 alert('Boas vindas ao jogo do número secreto');
+
 let numeroMaximo = 5000;
 let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
-console.log(numeroSecreto)
+console.log(numeroSecreto);
+
 let chute;
 let tentativas = 1;
 
-while (chute != numeroSecreto) {
+// FEATURE: limite máximo de 10 tentativas
+let maxTentativas = 10;
+
+while (chute != numeroSecreto && tentativas <= maxTentativas) {
     chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
+
     if (chute == numeroSecreto) {
         break;
     } else {
         if (chute > numeroSecreto) {
-            alert(`O número secreto é menor que ${chute}`)
+            alert(`O número secreto é menor que ${chute}`);
         } else {
-            alert(`O número secreto é maior que ${chute}`)
+            alert(`O número secreto é maior que ${chute}`);
         }
-        tentativas++
+
+        tentativas++;
     }
 }
 
-let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
-alert(`Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
+if (chute == numeroSecreto) {
+    let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+    alert(`Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
+} else {
+    alert(`Você perdeu! O número secreto era ${numeroSecreto}.`);
+}
